5b1cabf81d28f05e49f66af5a0e18ee5cb1e898b
unknown <thedarkness355@gmail.com>
2020-01-11 17:11:24 +0100

    coreection bug mongoose

--- main.js
+++ main.js
@@ -3,8 +3,9 @@ const { TOKEN, PREFIX } = require("./config");
 const client = new Client({ disableEveryone: true });
 
 client.PREFIX = PREFIX;
-
+client.mongoose = require("./util/mongoose");
 client.commands = new Collection();
+
 client.commands.set("repeat", require("./commands/repeat.js"));
 client.commands.set("role", require("./commands/role.js"));
 client.commands.set("sinfo", require("./commands/sinfo.js"));
@@ -18,6 +19,7 @@ client.on("ready", () => require("./events/ready.js")(client));
 client.on("message", msg => require("./events/message.js")(client, msg));
 client.on("guildMemberAdd", member => require("./events/guildMemberAdd.js")(client, member));
 
+client.mongoose.init();
 client.login(TOKEN);
 client.on("error", console.error);
 client.on("warn", console.warn);
